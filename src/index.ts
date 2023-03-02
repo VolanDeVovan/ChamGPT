f7cef77e9c6a8d62f1cdc91aa06232bb9d587c67
Vladimir Kravchenko <volandevovan2004@gmail.com>
2023-03-02 21:26:58 +0200

    Move context to presets

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { ChatGPTAPI, ChatGPTUnofficialProxyAPI } from 'chatgpt'
 import storage from 'node-persist'
 import { Api } from 'telegram'
-import dedent from 'ts-dedent'
 import { openaiApiKey } from './config'
+import { russianPrisoner } from './presets'
 import telegram from './telegram'
 
 async function main() {
@@ -14,9 +14,7 @@ async function main() {
         apiKey: openaiApiKey
     })
 
-    let context = await openaiApi.sendMessage(dedent`
-        I want you to act as an Russian prison jargon translator, slurs adder. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in Russian. I want you to replace my simplified A0-level words and sentences with more slurs and prison jargon's words and sentences. Keep the meaning same, but make them more slurs. I want you to only reply the correction, the improvements and nothing else, do not write explanations. My first sentence is "привет"
-    `)
+    let context = await openaiApi.sendMessage(russianPrisoner)
 
     const client = await telegram.login()
     const me: any = await client.getMe();
---
+++ src/presets.ts
@@ -0,0 +1,10 @@
+import dedent from "ts-dedent";
+
+export const russianPrisoner = dedent`
+    I want you to act as an Russian prison jargon translator, slurs adder. 
+    I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in Russian. 
+    I want you to replace my simplified A0-level words and sentences with more slurs and prison jargon's words and sentences. 
+    Keep the meaning same, but make them more slurs. 
+    I want you to only reply the correction, the improvements and nothing else, do not write explanations. 
+    My first sentence is "привет"
+`
